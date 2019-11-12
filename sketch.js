aa2bff258cae9b41973dafbc90f018d77ce53597
Garrett Gunnell <gunnellg@oregonstate.edu>
2019-11-12 14:31:59 -0800

    Implements basic ship movement

--- sketch.js
+++ sketch.js
@@ -1,5 +1,5 @@
 var tiles = []
-
+var shipcoords = [0, 0]
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -28,9 +28,10 @@ function initializeGrid() {
     var x = (windowHeight / 15) * 10 - squareSize / 2;
     for (var j = 0; j < 11; ++j) {
       if (i == 5 && j == 5) {
-        tiles[i][j] = initializeSquare(x, y, squareSize, true)
+        shipcoords = [i, j]
+        tiles[i][j] = initializeTile(x, y, squareSize, true)
       } else {
-        tiles[i][j] = initializeSquare(x, y, squareSize, false)
+        tiles[i][j] = initializeTile(x, y, squareSize, false)
       }
       x += squareSize
     }
@@ -38,7 +39,7 @@ function initializeGrid() {
   }
 }
 
-function initializeSquare(x, y, size, ship) {
+function initializeTile(x, y, size, ship) {
   return {
     x: x,
     y: y,
@@ -50,10 +51,15 @@ function initializeSquare(x, y, size, ship) {
       square(this.x, this.y, this.size)
       if (this.ship) {
         fill(0)
+        textSize(this.size / 3)
         text("ship", this.x + 10, this.y + this.size / 2);
       }
     }
   }
+}
+
+function updateTile(tile, ship) {
+    tile.ship = ship
 }
 
 function drawGrid() {
@@ -62,4 +68,40 @@ function drawGrid() {
       tiles[i][j].draw();
     }
   }
+}
+
+function moveShip(direction) {
+    shiptile = tiles[shipcoords[0]][shipcoords[1]]
+    var i = shipcoords[0]
+    var j = shipcoords[1]
+
+    if (direction == 'west') {
+        updateTile(tiles[i][j], false)
+        updateTile(tiles[i][j - 1], true)
+        shipcoords = [i, j - 1]
+    } else if (direction == 'east') {
+        updateTile(tiles[i][j], false)
+        updateTile(tiles[i][j + 1], true)
+        shipcoords = [i, j + 1]
+    } else if (direction == 'north') {
+        updateTile(tiles[i][j], false)
+        updateTile(tiles[i - 1][j], true)
+        shipcoords = [i - 1, j]
+    } else if (direction == 'south') {
+        updateTile(tiles[i][j], false)
+        updateTile(tiles[i + 1][j], true)
+        shipcoords = [i + 1, j]
+    }
+}
+
+function keyTyped() {
+    if (key == 'a') {
+        moveShip('west')
+    } else if (key == 'd') {
+        moveShip('east')
+    } else if (key == 'w') {
+        moveShip('north')
+    } else if (key == 's') {
+        moveShip('south')
+    }
 }
