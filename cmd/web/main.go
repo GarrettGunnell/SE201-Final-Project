00fd5b0e5032b9f1554e1c9461fe182f36f60111
Garrett Gunnell <gunnellg@oregonstate.edu>
2019-11-19 01:12:21 -0800

    AAAaaaAAAA

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -3,6 +3,7 @@ package main
 import (
     "log"
     "net/http"
+		"os"
 )
 
 func main() {
@@ -15,6 +16,14 @@ func main() {
     mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
     log.Println("Starting server on :4000")
-    err := http.ListenAndServe(process.env.PORT, mux)
+    err := http.ListenAndServe(getPort(), mux)
     log.Fatal(err)
+}
+
+func getPort() string {
+  p := os.Getenv("PORT")
+  if p != "" {
+    return ":" + p
+  }
+  return ":4000"
 }
