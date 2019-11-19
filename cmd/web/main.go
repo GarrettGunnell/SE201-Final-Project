40c2c410c8d2fd0343e8b5e207a21837a148faa9
Garrett Gunnell <gunnellg@oregonstate.edu>
2019-11-18 23:10:34 -0800

    Runs server and distributes html to client

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,19 +1,18 @@
 package main
 
 import (
-	"log"
-	"net/http"
+    "log"
+    "net/http"
 )
 
-func home(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello from test"))
-}
-
 func main() {
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", home)
+    mux := http.NewServeMux()
+    mux.HandleFunc("/", home)
 
-	log.Println("Starting new server on: 4000")
-	err := http.ListenAndServe(":4000", mux)
-	log.Fatal(err)
+    fileServer := http.FileServer(http.Dir("../../ui/static/"))
+    mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
+    log.Println("Starting server on :4000")
+    err := http.ListenAndServe(":4000", mux)
+    log.Fatal(err)
 }
